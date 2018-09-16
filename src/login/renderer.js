c844b84b318651183512932822e66d275b2cd28e
nezmustafa123 <nezmustafa123@gmail.com>
2018-09-16 17:26:45 +0100

    feat(sass): (_loginSty.scss)
    Add new sass file to style the form

--- src/login/renderer.js
+++ src/login/renderer.js
@@ -4,7 +4,9 @@ import reactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props){
     super(props);
-    this.state = { isLoginOpen: true, isRegisterOpen: false};
+    this.state = { 
+     isLoginOpen: true, isRegisterOpen: false
+    };
     }
 
 
@@ -32,7 +34,7 @@ class App extends React.Component {
           
           <div className = "box-controller">
           
-          <div className = {"controller" + (this.state.isLoginOpen ? "selected-controller": "") onClick ={this.showLoginBox.bind(this)}>
+          <div className = {"controller" + (this.state.isLoginOpen ? "selected-controller": "")} onClick ={this.showLoginBox.bind(this)}>
             Login
           </div>
              <div className = {"controller" + (this.state.isRegisterOpen ? "selected-controller" : "")} onClick ={this.showRegisterBox.bind(this)}>
@@ -64,50 +66,117 @@ class LoginBox extends React.Component {
         super(props);
         this.state = {
             //create empty errors array inside the state
+            username : "", email : "", password : "",
             errors: []
         };
     }
 
-    //two methods show validation erroe
+    //two methods show validation error
     
+     submitLogin(e) {}
     
     showValidationErr(elm, msg) {
         //element represents user name or password
-        //grab previous state provide call back
-        this.setState((prevState) => ({errors: [...prevState.errors, {elm, msg}]}) )
+        //grab state/errors and concatenate to form new array
+        this.setState((prevState) => ({errors: [...prevState.errors, {elm, msg}]}) );
     }
     //takes the element which represents the element username input or password input
     clearValidationErr(elm){
-        
+        this.setState((prevState) => {
+            let newArr = [];
+            for(let err of prevState.errors){
+                if(elm != err.elm){
+                    newArr.push(err);
+                    //while not equal push curent element to new array
+                }
+            }
+            return newArr;
+        });
     }
     
     
     
+    
+    
+    onUsernameChange(e) {
+        this.setState({username: e.target.value});
+    }
+    
+    onEmailChange(e) {
+        this.setState({email: e.target.value});
+    }
+    
+    
+    onPasswordChange(e) {
+        this.setState({password: e.target.value});
+    }
+    
+    
  submitRegister(e){
-       
+       //submit the form when click the submit button
+     if(this.state.username == "") {
+         
+      this.showValidationErr("Username", "Usernme Cannot be empty!");
+         
+     } if (this.state.email == "") {
+         this.showValidationErr("email", "Usernme Cannot be empty!");
+     }  if (this.state.password == "")
+          return this.showValidationErr("password", "Usernme Cannot be empty!");
    }
     
-    
+    //take array look through it and check is theres any elements regarding username and passwprd
+}
     
     render() {
+        let usernameErr = null,
+            passwordErr = null, 
+            emailErr = null;
+        
+        for( let err of this.state.errors){
+            //loop through each error
+            if(err.elm == "username"){
+                UsernameErr = err.msg;
+                
+            } if (err.elm == "password") {
+                passwordErr = err.msg;
+            } if (err.elm == "email") {
+                emailErr = err.msg; //xurrent error message
+            }
+        }
+        
           return (
         //couple of input fields
         <div className = "inner-container">
            <div className = "header">
-             Login
+             Register
            </div>
            <div className="box">
 
              <div className="input-group">
              <label htmlFor="username"> UserName</label>
-             <input type = "text" name="username" placeholder="username" id="username" className="login-input"/>
+             <input type = "text" name="username" placeholder="username" id="username" className="login-input"/
+              onChange ={this.onUsernameChange.bind(this)}/>
+            <small className = "danger-error">{emailErr ? emailErr : ""}</small>
+
              </div>
    
              <div className="input-group">
+             <label htmlFor="email"> Email</label>
+             <input type = "email" name="email" className="login-input" placeholder="Email" id="Email"
+              onChange= {this.onEmailChange.bind(this)}
+             />
+            <small className = "danger-error">{emailErr ? emailErr : ""}</small>
+            </div>
+
+    
+         <div className="input-group">
              <label htmlFor="password"> Password</label>
-             <input type = "password" name="password" className="login-input" placeholder="Password" id="password"/>
+             <input type = "password" name="password" className="login-input" placeholder="Password" id="Password"
+              onChange= {this.onEmailChange.bind(this)}
+             />
+               <small className = "danger-error">{passwordErr ? passwordErr : ""}</small>
              </div>
-           
+            
 
              /* I want an onclick event here */
            <button type="button" className="login-btn" onClick ={this.submitLogin.bind(this)}>login</button>
@@ -128,7 +197,7 @@ class RegisterBox extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-
+           
         };
     }
 
@@ -149,10 +218,22 @@ class RegisterBox extends React.Component {
             label htmlFor = "username" > UserName < /label> <
             input type = "text"
             name = "username"
-            placeholder = "username"
-            id = "username" / >
+            placeholder = "Username"
+            id = "username" / 
+            onChange ={this.onUsernameChange.bind(this)}/>
+            <small className = "danger-error">{usernameErr ? usernameErr : ""}</small>
             <
             /div>
+
+
+            <div className="input-group">
+            <label htmlFor="email">Email</label>
+            <input type="text" name="email" className="login-input" placeholder="Email"/>
+             <small className = "danger-error">{emailErr ? emailErr : ""}</small>
+            </div>
+
+
+
 
             <
             div className = "input-group" >
@@ -162,9 +243,14 @@ class RegisterBox extends React.Component {
             name = "password"
             className = "login-input"
             placeholder = "Password"
-            id = "password" / >
-            <
-            /div>
+            id = "password" 
+            
+            onChange = {this.onEmailChange.bind(this)}
+   
+            / >
+            <small className = "danger-error">{passwordErr ? passwordErr : ""}</small>
+
+            </div>
 
 
             /* I want an onclick event here */
